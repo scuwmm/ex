9f62214fd5788c13ca936c5e9bb2dfc306a7ec64
scuwmm <scumingming@163.com>
2022-08-11 13:42:10 +0800

    ex: auth trade

--- trade/src/main/java/com/scumm/ex/controller/TradeController.java
+++ trade/src/main/java/com/scumm/ex/controller/TradeController.java
@@ -3,12 +3,14 @@ package com.scumm.ex.controller;
 import com.scumm.ex.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope
 public class TradeController {
     @Resource
     private AuthService authService;
@@ -16,9 +18,17 @@ public class TradeController {
     @Value("${server.port}")
     private String port;
 
+//    @Value("${config.name}")
+//    private String name;
+
     @GetMapping(value = "/trade/auth")
     public String getPayment() {
         return authService.auth();
     }
+
+//    @GetMapping(value = "/config/name")
+//    public String getName() {
+//        return name;
+//    }
 
 }
